7d8231d6d611a22a2d7ddbec61f1cba1237fc689
Mahzabin <mashrufa1997@yahoo.co.uk>
2023-04-04 23:39:40 +0100

    finished exercise a, b and nearly all of c

--- start_point/exercise_b_users.py
+++ start_point/exercise_b_users.py
@@ -55,12 +55,52 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print(users["Jonathan"]["twitter"])
+
 # 2. Get Erik's hometown
+print(users["Erik"]["home_town"])
+
 # 3. Get the list of Erik's lottery numbers
+print(users["Erik"]["lottery_numbers"])
+
 # 4. Get the species of Avril's pet Monty
+print(users["Avril"]["pets"][0])
+
 # 5. Get the smallest of Erik's lottery numbers
+eriks_lottery_numbers = users["Erik"]["lottery_numbers"]
+eriks_smallest_lotto_nums = min(eriks_lottery_numbers)
+print(eriks_smallest_lotto_nums)
+
 # 6. Return an list of Avril's lottery numbers that are even
+
+avrils_lottery_numbers = users["Avril"]["lottery_numbers"]
+avrils_even_lotto_nums = [num for num in avrils_lottery_numbers if num % 2 == 0]
+print(avrils_even_lotto_nums)
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+eriks_lottery_numbers.append(7)
+print(eriks_lottery_numbers)
+
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
+print(users)
+
 # 9. Add a pet dog to Erik called "fluffy"
+eriks_profile = users["Erik"]
+eriks_profile["pets"] = {
+    "name": "fluffy",
+    "species": "dog"
+}
+
+print(eriks_profile)
+print(users)
+
 # 10. Add another person to the users dictionary
+users["Lavanya"] = {
+    "home_town": "Glasgow",
+    "gender": "woman",
+    "height": "small",
+    "personality": "dramatic"
+}
+
+print(users)
--- start_point/exercise_c_uk.py
+++ start_point/exercise_c_uk.py
@@ -17,6 +17,27 @@ united_kingdom = [
 ]
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+wales = united_kingdom[1]
+wales["capital"] = "Cardiff"
+
+print(united_kingdom)
+
 # 2. Create a dictionary for Northern Ireland and add it to the `united_kingdom` list (The capital is Belfast, and the population is 1,811,000).
+united_kingdom = { "name": "Northern Ireland", "capital": "Belfast", "population": "1,811,00" }
+
+print(united_kingdom)
+
 # 3. Use a loop to print the names of all the countries in the UK.
+
+# for name in united_kingdom.values():
+#     print(name("name")) 
+
 # 4. Use a loop to find the total population of the UK.
+# total_population = 0
+
+# for population in united_kingdom:
+#     total_population += population("population")
+#     population["population"] = 0
+
+# print(f"{total_population} is the total population of the UK")
+# print(int(united_kingdom))
